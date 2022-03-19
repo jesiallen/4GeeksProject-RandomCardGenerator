132ce48fefc35fb4209db29d0be3576f1a16132b
jesiallen <justjesi2010@gmail.com>
2022-03-19 00:14:45 +0000

    Created randomly generated card with 52 possible.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = ["♦", "♥", "♠", "♣"];
+  let number = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let drawSuit = suit[Math.floor(Math.random() * suit.length)];
+  if (drawSuit == "♦" || drawSuit == "♥") {
+    document.querySelector(".container-fluid").style.color = "red";
+  }
+
+  let drawNumber = number[Math.floor(Math.random() * number.length)];
+
+  document.querySelector(".suitTop").innerHTML = drawSuit;
+  document.querySelector(".cardNumber").innerHTML = drawNumber;
+  document.querySelector(".suitBottom").innerHTML = drawSuit;
 };
